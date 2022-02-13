00813f5a2ebbcb0e572eb5d6287c9c9491b39b12
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-02-13 22:24:24 +0530

    Create JS1.js

---
+++ Java Script Program/JS1.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body onload="alert('Welcome to JavaScript')">
+    <h2>This is firt js code</h2>
+    <h1>usind inline script </h1>
+    
+</body>
+</html>
