c661d5e02ee86c76504b4013504697055e7b7fa5
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-02-13 22:25:11 +0530

    Create JS2.js

---
+++ Java Script Program/JS2.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>JS2</title>
+    <h2> MSg from HEAD</h2>
+    <script>
+        document.write("This msg from Head")
+    </script>
+</head>
+<body>
+    <h2>MSG from Body</h2>
+    <script>
+        document.write("This mgs from Body")
+    </script>
+</body>
+</html>
